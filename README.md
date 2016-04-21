acbfee2b54c501198c85f48fe043b363b231452b
Jarek Sochacki <sociaqui@gmail.com>
2016-04-21 10:31:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+announcer
+=========
+
+A Symfony project created on April 21, 2016, 8:14 am.
---
+++ src/SociaquiBundle/SociaquiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SociaquiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SociaquiBundle extends Bundle
+{
+}
